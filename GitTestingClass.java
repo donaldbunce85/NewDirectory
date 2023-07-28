8b014f50dd3eb355b7a89f13fe60c4d699ddb3ee
Donald Bunce <donald.bunce85@gmail.com>
2023-07-28 07:20:57 -0400

    Add a comment command

--- GitTestingClass.java
+++ GitTestingClass.java
@@ -1,6 +1,8 @@
 public class GitTestingClass {
+    
 
     public static void main(String[] args) {
-
+        String newMessage = "Hello, I've added something new here";
+        System.out.println(newMessage);
     }
 }
