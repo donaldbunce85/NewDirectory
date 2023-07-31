72c1a8a49ded2db846493ea411e71bfa73e19572
Donald Bunce <donald.bunce85@gmail.com>
2023-07-31 17:38:21 -0400

    Another add for the master

--- CommitMath.java
+++ CommitMath.java
@@ -1,10 +1,10 @@
-public class CommitMath {
-
-    public static void main(String[] args) {
-        addition(5,10);
-    }
-
-    public static double addition(int x, int y) {
-        return x + y;
-    }
-}
+//public class CommitMath {
+//
+//    public static void main(String[] args) {
+//        addition(5,10);
+//    }
+//
+//    public static double addition(int x, int y) {
+//        return x + y;
+//    }
+//}
