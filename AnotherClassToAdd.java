68bc64ef17584b8508e7c0c0e6c1ea9b4f3cc2e0
Donald Bunce <donald.bunce85@gmail.com>
2023-07-31 17:08:39 -0400

    Add Brent Hutson

--- AnotherClassToAdd.java
+++ AnotherClassToAdd.java
@@ -1,6 +1,6 @@
 public class AnotherClassToAdd {
 
     public static void main(String[] args) {
-
+        System.out.println("Sweet first branch bro");
     }
 }
--- Professor.java
+++ Professor.java
@@ -6,6 +6,8 @@ public class Professor extends Person {
     public static void main(String[] args) {
         Professor professor = new Professor();
         professor.setName("Rob Hutson");
+        Professor secondProfessor = new Professor();
+        secondProfessor.setName("Brenton Hutson");
 
         System.out.println(professor.getName());
     }
